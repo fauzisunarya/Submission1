067c695f75c2137f861cee75de50e6922354bb2a
fauzisunarya <fauzialdzikirullah@gmail.com>
2019-09-28 17:13:42 +0700

    version 1.0.0

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "submission1.database.windows.net";
+    $user = "fauzi";
+    $pass = "Kafaufau020699";
+    $db = "submission1";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
